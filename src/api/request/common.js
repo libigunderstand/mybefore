7f843d25e51f5d4c0ffdfbd8e1d158d9ccd31301
liguangrui <54014139+libigunderstand@users.noreply.github.com>
2023-05-26 19:19:55 +0800

    请求

--- src/api/request/common.js
+++ src/api/request/common.js
@@ -4,7 +4,7 @@
  * @Autor: Gary
  * @Date: 2022-05-20 17:41:22
  * @LastEditors: Gary
- * @LastEditTime: 2023-05-26 17:56:11
+ * @LastEditTime: 2023-05-26 19:19:41
  */
 import $http from "@/utils/httpRequest";
 
@@ -16,9 +16,19 @@ export default {
 		});
 	},
 
-	getConfigFile: function (param) {
+	/**
+	 * 获取配置
+	 */
+	getConfigs: function () {
 		return new Promise(function (resolve, reject) {
-			let response = $http.get("/myback/config", param);
+			let response = $http.get("/watermark/config");
+			resolve(response);
+		});
+	},
+
+	saveConfigs: function (params) {
+		return new Promise(function (resolve, reject) {
+			let response = $http.post("/watermark/config/update", params);
 			resolve(response);
 		});
 	},
